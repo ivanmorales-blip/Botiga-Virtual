337b5300fa0c9cae1280ba74e1e950beea85c59f
ivanmorales-blip <ivan.morales@inslapineda.cat>
2026-04-14 10:13:49 +0200

    cambios

---
+++ app/Models/PackImage.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PackImage extends Model
+{
+    use HasFactory;
+
+    protected $table = 'pack_images';
+
+    protected $fillable = [
+        'pack_id',
+        'image_path',
+        'order',
+    ];
+
+    /**
+     * The pack this image belongs to
+     */
+    public function pack()
+    {
+        return $this->belongsTo(Pack::class);
+    }
+}
